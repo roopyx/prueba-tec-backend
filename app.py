0ccb65c9db2923ae83044b9361170817d3bb26fc
roopyx <roopyx.1111@gmail.com>
2022-10-25 17:48:04 -0300

    insert data form from frontend

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from routes.forms import forms
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:my-secret-pw@127.0.0.1:3306/formsdb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
--- routes/forms.py
+++ routes/forms.py
@@ -1,3 +1,4 @@
+from ast import Try
 from flask import Blueprint, request
 from models.form import Form
 from utils.db import db
@@ -6,15 +7,12 @@ forms = Blueprint('forms', __name__)
 
 @forms.route("/new/form", methods=['POST'])
 def new_form():
-    # expansion=request.form['expansion']
-    # fecha=request.form['fecha']
-    # destino=request.form['destino']
-    # tonelaje=request.form['tonelaje']
+    request_form = request.get_json()
 
-    expansion= 'exp-llp4'
-    fecha='2022-10-25'
-    destino='Independencia-Santiago'
-    tonelaje=2.83
+    expansion = request_form['expansion']
+    fecha = request_form['fecha']
+    destino = request_form['destino']
+    tonelaje = request_form['tonelaje']
 
     new_form = Form(expansion, fecha, destino, tonelaje)
 
