914dd8bba79fc49b38e2565ec0d9015020c5eded
roopyx <roopyx.1111@gmail.com>
2022-10-27 10:02:38 -0300

    add query to filter form with year and month

--- routes/forms.py
+++ routes/forms.py
@@ -1,7 +1,10 @@
 from ast import Try
+from asyncio import tasks
 from flask import Blueprint, request
 from models.form import Form
 from utils.db import db
+from sqlalchemy import extract
+import json
 
 forms = Blueprint('forms', __name__)
 
@@ -21,9 +24,20 @@ def new_form():
 
     return "New Form saved"
 
-@forms.route("/filter/date")
+@forms.route("/filter/date", methods=['GET'])
 def filter_date():
+  dict_forms = {}
   mes = request.args.get('mes')
   año = request.args.get('año') 
-  formsFilter = Form.query(Form).filter(Form.fecha)
-  return formsFilter
+  formsFilter = Form.query.filter(extract('year', Form.fecha)==año).filter(extract('month', Form.fecha)==mes).all()
+  for row in formsFilter:
+    dict_forms["id"] = row.id
+    dict_forms["expansion"] = row.expansion
+    dict_forms["fecha"] = str(row.fecha)
+    dict_forms["destino"] = row.destino
+    dict_forms["tonelaje"] = row.tonelaje
+    print(json.dumps(dict_forms))
+  print(dict_forms)
+  return 'lsl'
+
+
