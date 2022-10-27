238449fd837e1d39758e377ce7b8e6d463c14b8f
roopyx <roopyx.1111@gmail.com>
2022-10-27 19:02:56 -0300

    implement requirements.txt

--- routes/forms.py
+++ routes/forms.py
@@ -1,15 +1,15 @@
-from array import array
-from flask import Blueprint, request
+from flask import Blueprint, request, abort
 from models.form import Form
 from utils.db import db
 from sqlalchemy import extract
-import json
-import ast
 
 forms = Blueprint('forms', __name__)
 
 @forms.route("/new/form", methods=['POST'])
 def new_form():
+
+  try:
+
     request_form = request.get_json()
 
     expansion = request_form['expansion']
@@ -24,22 +24,33 @@ def new_form():
 
     return "New Form saved"
 
+  except:
+
+    return abort(400)
+
 @forms.route("/filter/date", methods=['GET'])
 def filter_date():
-  dict_forms = {}
-  lista_dict_forms = []
-  
-  mes = request.args.get('mes')
-  año = request.args.get('año') 
-  
-  formsFilter = Form.query.filter(extract('year', Form.fecha)==año).filter(extract('month', Form.fecha)==mes).all()
 
-  for row in formsFilter:
-    dict_forms["id"] = row.id
-    dict_forms["expansion"] = row.expansion
-    dict_forms["fecha"] = str(row.fecha)
-    dict_forms["destino"] = row.destino
-    dict_forms["tonelaje"] = row.tonelaje
-    lista_dict_forms.append(dict_forms.copy())
+  try:
 
-  return lista_dict_forms
+    dict_forms = {}
+    lista_dict_forms = []
+    
+    mes = request.args.get('mes')
+    año = request.args.get('año') 
+    
+    formsFilter = Form.query.filter(extract('year', Form.fecha)==año).filter(extract('month', Form.fecha)==mes).all()
+
+    for row in formsFilter:
+      dict_forms["id"] = row.id
+      dict_forms["expansion"] = row.expansion
+      dict_forms["fecha"] = str(row.fecha)
+      dict_forms["destino"] = row.destino
+      dict_forms["tonelaje"] = row.tonelaje
+      lista_dict_forms.append(dict_forms.copy())
+
+    return lista_dict_forms
+
+  except:
+
+    return abort(400)
